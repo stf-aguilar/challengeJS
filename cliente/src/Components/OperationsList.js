9478708e19c5dbf0dc71067b33e9d744ffb7d3b2
stf-aguilar <silestagui@gmail.com>
2021-05-05 00:47:03 -0300

    finalización de la parte de cliente

--- cliente/src/Components/OperationsList.js
+++ cliente/src/Components/OperationsList.js
@@ -1,6 +1,48 @@
 import React from 'react';
 
-const OperationsList = () => {
+const OperationsList = ({operation, setOperation, operations, setListUpdated}) => {
+	
+	const handleDelete = (id) => {
+		const requestInit = {
+			method: 'DELETE'
+		}
+		fetch('http://localhost:9000/api/'+ id, requestInit)
+      	.then(res => res.text())
+      	.then(res => console.log(res));
+
+      	setListUpdated(true);
+	}
+
+	let{concepto, monto,fecha,tipo} = operation;
+
+	const handleUpdate = (id) => {
+		monto = parseInt(monto);
+		//validación de los datos
+		if(concepto === '' || monto <= 0 || tipo === false){
+			alert('Todos los campos deben completarse')
+			return
+		}
+
+		const requestInit = {
+			method: 'PUT',
+			headers:{'Content-Type':'application/json'},
+			body:JSON.stringify(operation)
+		}
+		fetch('http://localhost:9000/api/'+ id, requestInit)
+      	.then(res => res.text())
+      	.then(res => console.log(res));
+
+      	//reiniciando state de operation
+      	setOperation({
+      		concepto:'',
+    		monto:0,
+    		fecha:'',
+    		tipo:null
+      	})
+
+      	setListUpdated(true);
+
+	}
 	return (
 		<table className="table">
 			<thead>
@@ -8,15 +50,30 @@ const OperationsList = () => {
 					<th>Id</th>
 					<th>Concepto</th>
 					<th>Monto</th>
+					<th>Fecha</th>
+					<th>Tipo</th>
 				</tr>
 			</thead>
 
 			<tbody>
-				<tr>
-					<th>...</th>
-					<th>...</th>
-					<th>...</th>
-				</tr>
+				{operations.map(operation => (
+					<tr key={operation.id}>
+						<td>{operation.id}</td>
+						<td>{operation.concepto}</td>
+						<td>{operation.monto}</td>
+						<td>{operation.fecha}</td>
+						<td>{operation.tipo}</td>
+						<td>
+							<div className="mb-3">
+								<button onClick={() => handleDelete(operation.id)} className="btn btn-danger">Delete</button>
+							</div>
+							<div className="mb-3">
+								<button onClick={() => handleUpdate(operation.id)} className="btn btn-info">Update</button>
+							</div>
+						</td>
+					</tr>
+
+				))}
 			</tbody>
 		</table>
 		);
